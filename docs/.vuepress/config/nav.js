64928e7d350e02cc628c21c20da616304f8674be
Northword <44738481+northword@users.noreply.github.com>
2020-12-18 22:23:14 +0800

    修复几个安装部分的链接错误、加了目录页

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,14 +1,14 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '理论', link: '/pages/77a22e/' },
-  { text: '安装', link: '/pages/06c8b9/' },
+  { text: '安装', link: '/contents/linux/' },
   { text: 'Linux', link: '/pages/9810fa/' },
   { text: 'MatStudio', link: '/pages/2e10d2/' },
-  { text: 'VASP', link: '/pages/df30ef/' },
+  { text: 'VASP', link: '/contents/vasp/' },
 
   { text: '其他', items: [
     { text: '其他软件', link: '/pages/c0ca29/' },
-    { text: '异常解决', link: '/pages/d82842/' },
+    { text: '异常解决', link: '/contents/others/' },
   ]}
 ];
 
