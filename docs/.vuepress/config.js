c93b85f4bcfc6bdbd22fa8844cabc6da9dfced1f
Northword <44738481+northword@users.noreply.github.com>
2020-12-21 22:48:13 +0800

    修改配置文件结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,142 +1,29 @@
-const nav = require('./config/nav.js');
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   title: "计算模拟学习笔记",
-  description: "计算模拟学习笔记", // 描述,以 <meta> 标签渲染到页面html中
+  description: "「能源环境材料计算模拟」方法课程学习与实践笔记，内容包括 Linux基础命令、PBS作业管理系统、VASP、VASPKIT、Materials Studio 等。", // 描述,以 <meta> 标签渲染到页面html中
   base: '/dft-learning/',          // '/<github仓库名>/'， 默认'/'
+  host: 'localhost',
+  extraWatchFiles: [
+    '.vuepress/config/nav.js', 
+    '.vuepress/config/head.js',
+    '.vuepress/config/themeConfig.js',
+  ],
   markdown: {
     lineNumbers: true,             // 代码行号
   },
 
   theme: 'vdoing', // 使用依赖包主题
-   //theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  //theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  themeConfig: {
-    // 主题配置
-    nav,
-    sidebarDepth: 2,                          // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png',                    // 导航栏logo
-    repo: 'northword/dft-learning',           // 导航栏右侧生成Github链接
-    searchMaxSuggestions: 10,                 // 搜索结果显示最大数
-    lastUpdated: '上次更新',            // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs',                          // 编辑的文件夹
-	docsBranch: 'main',
-    editLinks: true,                          // 启用编辑
-    editLinkText: '编辑文档！',
-	activeHeaderLinks: true,
-    //*** 以下配置是Vdoing主题改动和新增的配置 ***//
-	
-	// 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-    // category: false, 
-	
-	// 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
-    // tag: false, 
-	
-	// 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
-    // archive: false, 
-	
-	// 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
-    // categoryText: '随笔', 
+  head,
+  plugins,
+  themeConfig,
 
-    // bodyBgImg: [
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
-    // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
 
-    // titleBadge: false, // 文章标题前的图标是否显示，默认true
-    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
-    //   '图标地址1',
-    //   '图标地址2'
-    // ],
-	
-    // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 => 方格 | 2 => 横线 | 3 => 竖线 | 4 => 左斜线 | 5 => 右斜线 | 6 => 点状
-
-     updateBar: {                    // 最近更新栏
-       showToArticle: false,         // 显示到文章页底部，默认true
-    //   moreArticle: '/archives'        // “更多文章”跳转的页面，默认'/archives'
-     },
-	
-	rightMenuBar: false,             // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
-    // sidebarOpen: false,           // 初始状态是否打开侧边栏，默认true
-    pageButton: false,               // 是否显示快捷翻页按钮，默认true
-
-	// 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页	
-    sidebar: { mode: 'structuring', collapsable: false}, 
-
-    author: {
-      // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-      name: 'Northword', // 必需
-      link: 'https://github.com/northword', // 可选的
-    },
-    blogger: {
-      // 博主信息，显示在首页侧边栏
-      avatar: '/dft-learning/img/avatar.ico',
-      name: 'northword',
-      slogan: '菜鸡小张在此！',
-    },
-    social: {
-      // 社交图标，显示于博主信息栏和页脚栏
-      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-      icons: [
-        {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:zhangjianbei@stu.scu.edu.cn',
-        },
-        {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/northword',
-        },
-        {
-          iconClass: 'icon-erji',
-          title: '听音乐',
-          link: 'http://music.163.com/playlist/596573426/418115654/?userid=418115654',
-        },
-      ],
-    },
-    footer: {
-      // 页脚信息
-      createYear: 2020, // 博客创建年份
-      copyrightInfo:
-        'Northword', // 博客版权信息，支持a标签
-    },
-  },
-  
-  
-  plugins: [
-    // 插件
-    [
-      '@vuepress/last-updated', // "上次更新"时间格式
-      {
-        transformer: (timestamp, lang) => {
-          const moment = require('moment') // https://momentjs.com/
-          return moment(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-        },
-      },
-    ],
-    [
-      'one-click-copy',
-      {
-        // 代码块复制按钮
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功',       // default is 'Copy successfully and then paste it for use.'
-        duration: 1000,                // prompt message display time.
-        showInMobile: false,           // whether to display on the mobile side, default: false.
-      },
-    ],
-    [
-      'vuepress-plugin-zooming', // 放大图片
-      {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
-        options: {
-          bgColor: 'rgba(0,0,0,0.6)',
-        },
-      },
-    ],
-  ],
   // configureWebpack: {
   //   //webpack别名 如![Image from alias](~@alias/image.png)
   //   resolve: {
